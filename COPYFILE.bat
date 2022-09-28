ffc04b561c48815a6359b415eda2e0ed2f1d412a
Alvaro-SP <socop2412@gmail.com>
2022-09-27 23:03:15 -0600

    cleaning

--- COPYFILE.bat
+++ COPYFILE.bat
@@ -1,10 +1,10 @@
 
 chcp 65001
 @REM C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\P4.asm
-copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\P4.asm" "C:\MASM\MASM611\BIN\P4.asm"
-copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\MACROS.inc" "C:\MASM\MASM611\BIN\MACROS.inc"
-copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PRACTICAS\4\ACE1-222S0778A202000194PRAC4\ARCHIVOS.inc" "C:\MASM\MASM611\BIN\ARCHIVOS.inc"
-DEL /F /A "C:\MASM\MASM611\BIN\P4.exe"
+copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PROYECTOS\2\ACE1-222S0778A202000194PROY2\PROY2.asm" "C:\MASM\MASM611\BIN\PROY2.asm"
+copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PROYECTOS\2\ACE1-222S0778A202000194PROY2\MACP2.inc" "C:\MASM\MASM611\BIN\MACP2.inc"
+copy "C:\Users\alvaro\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\6 S E X T O   S E M E S T R E\arquiiiiiiii  1\LAB ARQUI\PROYECTOS\2\ACE1-222S0778A202000194PROY2\FILEP2.inc" "C:\MASM\MASM611\BIN\FILEP2.inc"
+DEL /F /A "C:\MASM\MASM611\BIN\PROY2.exe"
 cd "C:\Program Files (x86)\DOSBox-0.74-3\"
 DOSBox.exe
 @REM pause
--- commit with MESSAGE.bat
+++ commit with MESSAGE.bat
@@ -1,7 +1,7 @@
 
 @echo off
 echo %~dp0
-git remote add all https://github.com/Alvaro-SP/ACE1-222S0778A202000194PROY2.git
+@REM git remote add all https://github.com/Alvaro-SP/ACE1-222S0778A202000194PROY2.git
 
 SET /P NOMBRE=WRITE COMMIT:
 git add .
