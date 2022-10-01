baf7050081bfb1a98dee7f26b63bf65c3bd963df
Alvaro-SP <socop2412@gmail.com>
2022-10-01 17:03:46 -0600

    ADDING DRAWS OF FIGURES

--- PROY2.asm
+++ PROY2.asm
@@ -33,6 +33,7 @@ INCLUDE MACP2.inc
         POSX            DB  ?
         POSY            DB  ?
         saltolinea      DB " ",10, 13, "$"
+        txtmayor      DB " > ", "$"
         X1              DW  ?
         X2              DW  ?
         Y1              DW  ?
@@ -45,30 +46,132 @@ INCLUDE MACP2.inc
     ;*--------------------------  MENSAJES -----------------------------
         ingpath db "Ingrese la ruta path del archivo:", 10, 13, "$"
         EXITO db "EXITO. ARCHIVO GUARDADO CON EXITO CARPETA BIN", 10, 13, "$"
+        overflow db 00h
+        numberF       dB ?
     ;* --------------------------  REPORTES -----------------------------
         Filenamejug1  db  'Rep.xml'
         handlerentrada dw ?
-        handler   dw ?
+        handler     dw ?
         hour        db "00", "$"
         min         db "00", "$"
         sec         db "00", "$"
-        mes      db "00", "$"
-        dia      db "00", "$"
+        mes         db "00", "$"
+        dia         db "00", "$"
         SI_SIMULADO             DW ?
         SI_SIMULADO2             DW ?
+    ;? ------------------------BUBBLESORT VARIABLES------------------------
+        listestadistic          dw 2000 dup('$')
+        indexbbsort             DW 0000
+        RESULTADOPRINT          dw 00h, '$'
+
+    ;!-------------------------- VAR DEL JUEGO --------------------------
+    Xtemp               DW      ?
+    Ytemp               DW      ?
+    X2temp              DW      ?
+    Y2temp              DW      ?
+    Xaux1              DW      ?    ;* posiciones de paint
+    Yaux1              DW      ?
+    Xaux2              DW      ?
+    Yaux2              DW      ?
+    Xtempauxaux              DW      ?
+    Ytempauxaux              DW      ?
+    coloraux           DB      ? ;* coloar auxiliar cuadro
+    ;? --------------------------   COLORES   --------------------------
+    GREEN               EQU  02H
+    BLUE                EQU  01H
+    GREEN               EQU  02H
+    CYAN                EQU  03H
+    RED                 EQU  04H
+    MAGENTA             EQU  05H
+    BROWN               EQU  06H
+    LIGHT_GRAY          EQU  07H
+    DARK_GRAY           EQU  08H
+    LIGHT_BLUE          EQU  09H
+    LIGHT_GREEN         EQU  0AH
+    LIGHT_CYAN          EQU  0BH
+    LIGHT_RED           EQU  0CH
+    LIGHT_MAGENTA       EQU  0DH
+    YELLOW              EQU  0EH
+    WHITE               EQU  0FH
 ;!████████████████████████████ SEGMENTO DE CODIGO ████████████████████████████
 .CODE
 
-    main PROC FAR
+    main PROC FAR ; MAINMAIN
         MOV AX, @DATA
         MOV DS, AX
         MOV ES, AX
+        misdatos
+        esperaenter  ;TODO: activar despues
+        paint  0, 0, 800, 600, BLACK ;*LIMPIA TODO MODO VIDEO:V
+        ; menu
+        ; esperaenter
+        PINTARPANTALLADEJUEGO
+        ; MOV Xtemp,540
+        ; MOV Ytemp, 540+16
+        ; MOV X2temp,144
+        ; MOV Y2temp,144+16
+        ; DRAW_RECTANGLE  Xtemp,X2temp,Ytemp,Y2temp,GREEN ;* CorX, CorY, Cor2X, Cor2Y
+        ; delay 5000, 1000
+        ; DRAW_RECTANGLE  455,292,473,310,BLACK
+        ; DRAW_RECTANGLE  455,292+18,473,310+18,GREEN
+        ; delay 5000, 1000
+        ; DRAW_RECTANGLE  455,292+18,473,310+18,BLACK
+        ; DRAW_RECTANGLE  455,292+18+18,473,310+18+18,GREEN
+                ; ;*PRUEBA BUBBLE SORT
+                        ; MOV listestadistic[0], 88
+                        ; MOV listestadistic[2], 2
+                        ; MOV listestadistic[4], 88
+                        ; MOV listestadistic[6], 8
+                        ; MOV listestadistic[8], 8
+                        ; MOV listestadistic[10], 9
+                        ; MOV listestadistic[12], 80
+                        ; MOV listestadistic[14], 0
+
+                        ; MOV indexbbsort,16
+
+                        ; BUBBLESORT
+                        ; MOV AX,listestadistic[0]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[2]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[4]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[6]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[8]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[10]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[12]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; MOV AX,listestadistic[14]
+                        ; printnum RESULTADOPRINT, AX
+                        ; print RESULTADOPRINT
+                        ; print saltolinea
+                        ; readtext
+
+
+        
         SALIDADEUNA:
             mov ax, 4c00h
             int 21h
             HLT ; para decirle al CPU que se estara ejecutando varias veces (detiene CPU hasta sig interrupcion)
             RET
-    main    ENDP
+    main    ENDP 
 
     ;?☻ ===================== CONCATENAR TEXTO ENTRADA ======================= ☻
     readtext_ PROC NEAR
@@ -95,56 +198,6 @@ INCLUDE MACP2.inc
         Salir:
         RET
     readtext_ ENDP
-
-    ;*        █████████████████████████████████████████████████████████████████████
-    ;*        █▄─▄▄─█─▄▄▄▄█─▄─▄─██▀▄─██▄─▄▄▀█▄─▄█─▄▄▄▄█─▄─▄─█▄─▄█─▄▄▄─██▀▄─██─▄▄▄▄█
-    ;*        ██─▄█▀█▄▄▄▄─███─████─▀─███─██─██─██▄▄▄▄─███─████─██─███▀██─▀─██▄▄▄▄─█
-    ;*        ▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀
-
-
-    BUBBLESORT_ PROC NEAR
-        ; MOV SI,INDEXlistestadistic
-        ; DEC SI
-        ; DEC SI
-        ; MOV TAMANODEARRAY, SI
-        ; MOV CX, TAMANODEARRAY
-        ; MOV SI, -2
-        ; MOV DI, -2
-        ; ILOOP:       ;* for i in range(0,len(list1)-1):
-        ;     INC SI
-        ;     INC SI
-        ;     JLOOP:  ;* for j in range(len(list1)-1)
-        ;         INC DI
-        ;         INC DI
-        ;         CONDITION:          ;*if(list1[j]>list1[j+1]):
-
-        ;             MOV AX, listestadistic[DI]
-        ;             MOV BX, listestadistic[DI+2]
-        ;             CMP AX,BX
-        ;             JG MAYORQUE
-        ;             JMP JLOOP_
-        ;             MAYORQUE:
-        ;                 MOV AX, listestadistic[DI]
-        ;                 MOV BX, listestadistic[DI+2]
-        ;                 MOV listestadistic[DI], BX
-        ;                 MOV listestadistic[DI+2], AX  ;*list1[j+1] = temp
-        ;                 JMP JLOOP_
-        ; JLOOP_:
-        ;     CMP DI, CX
-        ;     JNE JLOOP
-        ;     JE ILOOPS_
-        ; ILOOPS_:
-        ;     MOV DI,-2
-        ;     cmp SI, CX
-        ;     JNE ILOOP
-        ;     JE SALIR
-
-
-        ; SALIR:
-        RET
-    BUBBLESORT_ ENDP
-
-
 
     ;?☻ ===================== POSICIONAR EL CURSOR ======================= ☻
     poscursor_ PROC NEAR
@@ -223,6 +276,290 @@ INCLUDE MACP2.inc
         JNZ DRAW_ALL_RECTANGLE_ROWS
         RET
     paint_ ENDP
+    DRAW_RECTANGLE_   PROC  NEAR    
+        ;PARAMETERS
+        ; X1, Y1, X2, Y2, AL = COLOR
+        INC X2
+        INC Y2  ;TO STOP AT X2 + 1, Y2 + 1
+        MOV DX, Y1
+        MOV AH, 0CH   ;AH = 0C FOR INT, AL = COLOR
+        DRAW_ALL_RECTANGLE_ROWS:
+        MOV CX, X1
+            DRAW_RECTANGE_ROW:
+                INT 10H
+                INC CX
+                CMP CX, X2
+            JNZ DRAW_RECTANGE_ROW
+        INC DX
+        CMP DX, Y2
+        JNZ DRAW_ALL_RECTANGLE_ROWS
+        RET
+    DRAW_RECTANGLE_ ENDP
+    PINTARPANTALLADEJUEGO_ PROC NEAR
+        DRAW_RECTANGLE 468,144,656, 144+18, CYAN
+        DRAW_RECTANGLE 468,144,468+18, 484, CYAN
+        DRAW_RECTANGLE 468,484-18,656, 484, CYAN
+        DRAW_RECTANGLE 656-18,144,656, 484, CYAN
+        ;PINTAR TITULO
+        DRAW_RECTANGLE 144+18,108+36,144+36, 108+36+18, WHITE
+        DRAW_RECTANGLE 54,108,324, 108+18, WHITE
+        DRAW_RECTANGLE  90,108+18,90+18, 216, WHITE             ; PALABRA TETRIS
+        DRAW_RECTANGLE  144,108+18,144+18, 216, WHITE
+        DRAW_RECTANGLE  144,216-18,198, 216, WHITE
+        DRAW_RECTANGLE  144+18,216-18,144+36, 216-18, WHITE
+        DRAW_RECTANGLE  216,108+18,216+18, 216, WHITE
+        DRAW_RECTANGLE  270,108+18,270+18, 216, WHITE
+        DRAW_RECTANGLE  270+18,108+36,270+36, 108+36+18, WHITE
+        DRAW_RECTANGLE  270+36,108+18,270+36+18, 108+36, WHITE
+        DRAW_RECTANGLE  270+36,108+36+18,270+36+18, 216, WHITE
+        DRAW_RECTANGLE  342,108,342+18, 216, WHITE
+        DRAW_RECTANGLE  378,108,414, 108+18, WHITE
+        DRAW_RECTANGLE  378,108+18,378+18, 108+18+18, WHITE
+        DRAW_RECTANGLE  378,108+36,414, 108+36+18, WHITE
+        DRAW_RECTANGLE  414-18,108+(18*3),414, 216, WHITE
+        DRAW_RECTANGLE  378,216-18,414,216, WHITE
+        DRAW_RECTANGLE  486, 162, 638, 466, BLUE
+        PINTARCUADRO 684,162
+        PINTARPALITO 720, 72
+        PINTARTE 720,216
+        PINTAR1ELE 666, 288
+        PINTAR2ELE 756, 360
+        PINTAR1ZETA 680, 450
+        PINTAR2ZETA 720, 522
+        RET
+    PINTARPANTALLADEJUEGO_ ENDP
+    PINTARCUADRO_ PROC NEAR;! CUADRADO
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_CYAN
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_CYAN
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_CYAN
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1,LIGHT_CYAN
+        RET
+    PINTARCUADRO_ ENDP
+    
+    PINTARPALITO_ PROC NEAR;! PALITO
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        RET
+    PINTARPALITO_ ENDP
+    PINTARPALITO2_ PROC NEAR;! PALITO2
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GREEN
+        RET
+    PINTARPALITO2_ ENDP
+    PINTARTE_ PROC NEAR;! TE
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_RED
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_RED
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_RED
+        MOV CX, Xtemp
+        ADD CX, 40
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_RED
+        RET
+    PINTARTE_ ENDP
+    PINTAR1ELE_ PROC NEAR;! ELE1
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, MAGENTA
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, MAGENTA
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, MAGENTA
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, MAGENTA
+        RET
+    PINTAR1ELE_ ENDP
+    PINTAR2ELE_ PROC NEAR;! ELE2
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, YELLOW
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, YELLOW
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, YELLOW
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, YELLOW
+        RET
+    PINTAR2ELE_ ENDP
+    PINTAR1ZETA_ PROC NEAR;! ELE1
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GRAY
+        MOV CX, Xtemp
+        ADD CX, 40
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GRAY
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GRAY
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, LIGHT_GRAY
+        RET
+    PINTAR1ZETA_ ENDP
+    PINTAR2ZETA_ PROC NEAR;! ELE2
+        MOV CX, Xtemp
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, BROWN
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, BROWN
+        MOV CX, Xtemp
+        ADD CX, 20
+        MOV Xaux1, CX
+        MOV CX, Ytemp
+        ADD CX, 20
+        MOV Yaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, BROWN
+        MOV CX, Xtemp
+        ADD CX, 40
+        MOV Xaux1, CX
+        PAINTSQUARE  Xaux1,Yaux1, BROWN
+        RET
+    PINTAR2ZETA_ ENDP
+
+    PAINTSQUARE_ PROC NEAR          ;! -----------PINTAR UN CUADRO 18*18-----------
+        MOV CX, Xtempauxaux
+        MOV Xaux1, CX
+        ADD CX, 18
+        MOV Xaux2, CX
+
+        MOV CX, Ytempauxaux
+        MOV Yaux1, CX
+        ADD CX, 18
+        MOV Yaux2, CX
+        DRAW_RECTANGLE  Xaux1,Yaux1,Xaux2,Yaux2, coloraux
+        RET
+    PAINTSQUARE_ ENDP
+    
+    ;*        ████████████████████████████████ ████████████████████████████████████
+    ;*        █▄─▄▄─█─▄▄▄▄█─▄─▄─██▀▄─██▄─▄▄▀█▄─▄█─▄▄▄▄█─▄─▄─█▄─▄█─▄▄▄─██▀▄─██─▄▄▄▄█
+    ;*        ██─▄█▀█▄▄▄▄─███─████─▀─███─██─██─██▄▄▄▄─███─████─██─███▀██─▀─██▄▄▄▄─█
+    ;*        ▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀
+
+
+    BUBBLESORT_ PROC NEAR
+        MOV CX, indexbbsort
+        DEC CX
+        DEC CX
+        DEC CX
+        DEC CX
+        MOV SI, -2
+        MOV DI, -2
+        ILOOP:       ;* for i in range(0,len(list1)-1):
+            INC SI
+            INC SI
+            JLOOP:  ;* for j in range(len(list1)-1)
+                INC DI
+                INC DI
+                CONDITION:          ;*if(list1[j]>list1[j+1]):
+                    MOV AX, listestadistic[DI]
+                    MOV BX, listestadistic[DI+2]
+                    CMP AX,BX
+                    Ja MAYORQUE  ;! JB para top ten
+                    JMP JLOOP_
+                    MAYORQUE:
+                        MOV AX, listestadistic[DI]
+                        MOV BX, listestadistic[DI+2]
+                        MOV listestadistic[DI], BX
+                        MOV listestadistic[DI+2], AX  ;*list1[j+1] = temp
+                        JMP JLOOP_
+        JLOOP_:
+            CMP DI, CX
+            JNE JLOOP
+            JE ILOOPS_
+        ILOOPS_:
+            MOV DI,-2
+            cmp SI, CX
+            JNE ILOOP
+            JE SALIR
+        SALIR:
+        RET
+    BUBBLESORT_ ENDP
+
     
     ;!  █▀█ █▀▀ █▀█ █▀█ █▀█ ▀█▀ █▀
     ;!  █▀▄ ██▄ █▀▀ █▄█ █▀▄ ░█░ ▄█☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻ GENERACION DE REPORTES
--- commit with MESSAGE.bat
+++ commit with MESSAGE.bat
@@ -8,6 +8,6 @@ git add .
 git commit -m "%NOMBRE%"
 git push
 git push https://Alvaro-SP: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@github.com/Alvaro-SP/ACE1-222S0778A202000194PROY2.git
-@REM pause
+pause
 
 
