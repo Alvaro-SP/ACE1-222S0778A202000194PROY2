917de7981f2155e70104c69064cf82635b374a70
Alvaro-SP <socop2412@gmail.com>
2022-10-07 17:50:07 -0600

    MENU ADMIN AND USER

--- PROY2.asm
+++ PROY2.asm
@@ -334,6 +334,9 @@ INCLUDE MACP2.inc
             HEAPSORT
             JMP Inicio
         QuickSortLB:
+            paint  0, 0, 800, 600, GREEN
+            paint  0, 0, 800, 600, BLACK
+            QUICKSORT
             JMP Inicio
         FIN:
         RET
@@ -821,7 +824,13 @@ INCLUDE MACP2.inc
         RET
     BUBBLESORT_ ENDP
 
+    HEAPSORT_ PROC NEAR
     
+        RET
+    HEAPSORT_ ENDP
+    QUICKSORT_ PROC NEAR
+        RET
+    QUICKSORT_ ENDP
     ;!  █▀█ █▀▀ █▀█ █▀█ █▀█ ▀█▀ █▀
     ;!  █▀▄ ██▄ █▀▀ █▄█ █▀▄ ░█░ ▄█☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻☻ GENERACION DE REPORTES
 
@@ -848,3 +857,54 @@ end     MAIN
 ;*  ██╔══██║██║░░░░░░╚████╔╝░██╔══██║██╔══██╗██║░░██║    ░╚═══██╗██║░░██║██║░░██╗██║░░██║██╔═══╝░
 ;*  ██║░░██║███████╗░░╚██╔╝░░██║░░██║██║░░██║╚█████╔╝    ██████╔╝╚█████╔╝╚█████╔╝╚█████╔╝██║░░░░░
 ;*  ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░    ╚═════╝░░╚════╝░░╚════╝░░╚════╝░╚═╝░░░░░
+
+
+
+;! ANEXOS
+;*  HEAPSORT     Algorithm      BASE:
+
+;*    /* sorts the given array of n size */
+;*    void heapsort(int* arr, int n)
+;*    {
+;*        // build the binary max heap
+;*        for (int i = n / 2 - 1; i >= 0; i--)
+;*        {
+;*            heapify(arr, n, i);
+;*        }
+;*        // sort the max heap
+;*        for (int i = n - 1; i >= 0; i--)
+;*        {
+;*            // swap the root node and the last leaf node
+;*            int temp = arr[i];
+;*            arr[i] = arr[0];
+;*            arr[0] = temp;
+;*            // again heapify the max heap from the root
+;*            heapify(arr, i, 0);
+;*        }
+;*    }
+;*    /* heapify the subtree with root i */
+;*    void heapify(int* arr, int n, int i)
+;*    {
+;*        // store largest as the root element
+;*        int largest = i;
+;*        int left = 2 * i + 1;
+;*        int right  = 2 * i + 2;
+;*        // now check whether the right and left right is larger than the root or not
+;*        if (left < n && arr[left] > arr[largest])
+;*        {
+;*            largest = left;
+;*        }
+;*        if (right < n && arr[right] > arr[largest])
+;*        {
+;*            largest = right;
+;*        }
+;*        // if the root is smaller than the children then swap it with the largest children's value
+;*        if (largest != i)
+;*        {
+;*            int temp = arr[i];
+;*            arr[i] = arr[largest];
+;*            arr[largest] = temp;
+;*            // again heapify that side of the heap where the root has gone
+;*            heapify(arr, n, largest);
+;*        }
+;*    }
