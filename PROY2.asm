79c602bba318ae66e0243ce2a3f461eae0a06b9d
Alvaro-SP <socop2412@gmail.com>
2022-10-07 20:57:21 -0600

    MENU ADMIN AND USER

--- PROY2.asm
+++ PROY2.asm
@@ -825,9 +825,57 @@ INCLUDE MACP2.inc
     BUBBLESORT_ ENDP
 
     HEAPSORT_ PROC NEAR
-    
+        ;*    /* sorts the given array of n size */
+        ;*    void heapsort(int* arr, int n)
+        ;*    {
+        ;*        // build the binary max heap
+        ;*        for (int i = n / 2 - 1; i >= 0; i--)
+        ;*        {
+        ;*            heapify(arr, n, i);
+        ;*        }
+        ;*        // sort the max heap
+        ;*        for (int i = n - 1; i >= 0; i--)
+        ;*        {
+        ;*            // swap the root node and the last leaf node
+        ;*            int temp = arr[i];
+        ;*            arr[i] = arr[0];
+        ;*            arr[0] = temp;
+        ;*            // again heapify the max heap from the root
+        ;*            heapify(arr, i, 0);
+        ;*        }
+        ;*    }
         RET
     HEAPSORT_ ENDP
+    HEAPIFY_ PROC NEAR
+        ;*    void heapify(int* arr, int n, int i)
+        ;*    {
+        ;*        // store largest as the root element
+        ;*        int largest = i;
+        ;*        int left = 2 * i + 1;
+        ;*        int right  = 2 * i + 2;
+        ;*        // now check whether the right and left right is larger than the root or not
+        ;*        if (left < n && arr[left] > arr[largest])
+        ;*        {
+        ;*            largest = left;
+        ;*        }
+        ;*        if (right < n && arr[right] > arr[largest])
+        ;*        {
+        ;*            largest = right;
+        ;*        }
+        ;*        // if the root is smaller than the children then swap it with the largest children's value
+        ;*        if (largest != i)
+        ;*        {
+        ;*            int temp = arr[i];
+        ;*            arr[i] = arr[largest];
+        ;*            arr[largest] = temp;
+        ;*            // again heapify that side of the heap where the root has gone
+        ;*            heapify(arr, n, largest);
+        ;*        }
+        ;*    }
+        RET
+    HEAPIFY_ ENDP
+
+
     QUICKSORT_ PROC NEAR
         RET
     QUICKSORT_ ENDP
