b52a60ed426cbec13e9d62159c99fba0b7fe1674
Alvaro-SP <socop2412@gmail.com>
2022-10-12 19:03:59 -0600

    PROCES LOGIC LIMITS OF ROTATIONS

--- COMMIT.bat
+++ COMMIT.bat
@@ -3,7 +3,7 @@
 echo %~dp0
 
 git add .
-git commit -m "PROCES LOGIC OF THE LIMITS OF FIGURES"
+git commit -m "PROCES LOGIC LIMITS OF ROTATIONS"
 git push
 git push https://Alvaro-SP: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@github.com/Alvaro-SP/ACE1-222S0778A202000194PROY2.git
 @REM pause
